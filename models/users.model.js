f135bc45762f10b005295fb8e238b06bec65dd2a
cahyoganteng <cahyormbl470@gmail.com>
2019-03-16 19:45:22 +0700

    crud user

--- models/users.model.js
+++ models/users.model.js
@@ -2,9 +2,30 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
+    id: {
+        type: String,
+        unique: true,
+        required: true
+    },
     name: String,
     phone: String,
-    email: String
+    email: String,
+    balance: Number,
+    verified: Boolean,
+    vouchers: [{
+        code: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        amount: Number,
+        expired: {
+            type: Date,
+            default: Date.now
+        },
+        receiver: String,
+        claimed: Boolean
+    }]
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/users.model');
+var uuid = require('uuid');
+// var nodemailer = require('nodemailer');
+// var async = require('async');
+// var crypto = require('crypto');
 // var userController = require('../controllers/user.controller');
 
 //GET
@@ -17,9 +21,39 @@ router.get('/users', (req, res) => {
     })
 })
 
+// //Verifikasi
+// router.get('/users/verifikasi-email/:token', (req, res) => {
+//     User.findOneAndUpdate({
+//             emailVerificationToken: req.params.token
+//         }, {
+//             $set: {
+//                 emailConfirmed: 'yes'
+//             },
+//             $unset: {
+//                 emailVerificationToken: ''
+//             }
+//         }, {
+//             save: true,
+//             upsert: false
+//         },
+//         function (err, user) {
+//             if (err) {
+//                 res.send(err);
+//             }
+//             if (user.role == 'guru') {
+//                 req.flash('SaveSucces', 'Terimakasih, Email anda telah diverifikasi');
+//                 req.redirect('/user');
+//             } else if (data.role == 'unverifed') {
+//                 res.redirect('/verifed-data');
+//             }
+//         }
+//     )
+// })
+
 //POST
 router.post('/user', (req, res) => {
     var user = new User();
+    user.id = uuid.v4();
     user.name = req.body.name;
     user.phone = req.body.phone;
     user.email = req.body.email;
@@ -28,6 +62,41 @@ router.post('/user', (req, res) => {
         if (err) return next(err);
         res.json('Succes create user');
     })
+
 })
 
+//UPDATE
+router.put('/user/:id', (req, res) => {
+    var updateUser = {
+        name: req.body.name
+    }
+
+    User.findOneAndUpdate({
+        id: req.params.id
+    }, updateUser, err => {
+        if (err) {
+            return res.json({
+                succes: false,
+                error: err
+            });
+        } else {
+            return res.json({
+                succes: true
+            });
+        }
+    });
+});
+
+//DELETE
+router.delete('/user/:id', (req, res) => {
+    var deleteById = {
+        id: req.params.id
+    }
+    User.findOneAndDelete(deleteById, err => {
+        if (err) return res.send(err);
+        return res.json({
+            succes: true
+        });
+    });
+});
 module.exports = router;
